1f89b829f53e3abd10b51a4af4229a6dfef520a2
rmarticedeno <rmarticedeno@gmail.com>
2020-09-06 11:50:04 -0400

    Add Example file

---
+++ Example.py
@@ -0,0 +1,10 @@
+from Extractor import Get_To_Excel
+
+#Channel
+Get_To_Excel('https://www.youtube.com/channel/UCx8GU_YJAUY80ch5MtuiJsA', 'Bachecubano')
+
+#Playlist
+Get_To_Excel('https://www.youtube.com/playlist?list=PLbpi6ZahtOH4OiptAsB2BrGjIiITwrdqs',"Life in a Day")
+
+#Video
+Get_To_Excel('https://www.youtube.com/watch?v=uIBp-eN4Ph4', "Music")
